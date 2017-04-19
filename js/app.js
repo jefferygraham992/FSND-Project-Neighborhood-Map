3044f1404cb785765f69ace8acd977812c2c3094
Jeffery Graham <jefferygraham992@gmail.com>
2017-04-19 11:42:04 -0400

    chore: refactor code for separation of concerns

--- js/app.js
+++ js/app.js
@@ -7,12 +7,21 @@ var locations = [
   {title: 'Flow Yoga Center', location: {lat: 38.910008, lng: -77.032288}}
 ];
 
+var map;
+var washingtonDC = {lat: 38.9072, lng: -77.0369};
+// Create a new blank array for all the listing markers.
+var markers = [];
 function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: washingtonDC,
-    zoom: 13
+    zoom: 12
   });
-
+  // These are the real estate listings that will be shown to the user.
+  // Normally we'd have these in a database instead.
+  var largeInfowindow = new google.maps.InfoWindow();
+  var bounds = new google.maps.LatLngBounds();
+  // The following group uses the location array to create an array of markers on initialize.
   for (var i = 0; i < locations.length; i++) {
     // Get the position from the location array.
     var position = locations[i].location;
@@ -27,8 +36,30 @@ function initMap() {
     });
     // Push the marker to our array of markers.
     markers.push(marker);
+    // Create an onclick event to open an infowindow at each marker.
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
+    bounds.extend(markers[i].position);
   }
-};
+  // Extend the boundaries of the map for each marker
+  map.fitBounds(bounds);
+}
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+  // Check to make sure the infowindow is not already opened on this marker.
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent('<div>' + marker.title + '</div>');
+    infowindow.open(map, marker);
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick',function(){
+      infowindow.setMarker = null;
+    });
+  }
+}
 
 function yogaLocation(value) {
   this.name = ko.observable(value.name);
