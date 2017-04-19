a4ff882af30a331b880356844108226ce42baf8a
Jeffery Graham <jefferygraham992@gmail.com>
2017-04-18 23:23:27 -0400

    feat: Import data into maps using geojson

---
+++ js/geoJSON.js
@@ -0,0 +1,27 @@
+eqfeed_callback({
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.016488,
+          38.876134
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.032288,
+          38.910008
+        ]
+      }
+    }
+  ]
+});
