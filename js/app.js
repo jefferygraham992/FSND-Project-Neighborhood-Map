410098a5eaeb92ee594256ca38ac58447fa1adbc
Jeffery Graham <jefferygraham992@gmail.com>
2017-04-20 16:46:31 -0400

    feat: Incorporate knockout

--- js/app.js
+++ js/app.js
@@ -1,38 +1,43 @@
-var map;
 var washingtonDC = {lat: 38.904174, lng: -77.017021};
 var url = "https://api.foursquare.com/v2/venues/explore?client_id=JG3FXNYMAHZG1OVUMBZACXPP3CBVLNT2X1O0BXKGOZKRO4SA%20&client_secret=XI2JWF5HUU2CUOLITHDB2NUZ3EZXEIYML5PVCOG12IZIWNU5%20&v=20130815%20&ll=38.904174,-77.017021&query=yoga&radius=10000&limit=10";
-var map;
-// Create a new blank array for all the listing markers.
-function initMap() {
-  // Constructor creates a new map - only center and zoom are required.
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: washingtonDC,
-    zoom: 13
+var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: new google.maps.LatLng(washingtonDC),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+$.ajax({
+  url:url,
+  type: "GET",
+  dataType: "json",
+  success:function(result) {
+    var locations = result.response.groups[0].items;
+    for (var i = 0; i < locations.length; i++) {
+     setMarker(locations[i]);
+    }
+ }
+});
+
+function setMarker(data) {
+  var infowindow = new google.maps.InfoWindow({
+    content: data.venue.name
   });
-  $.ajax({
-      url:url,
-      //async: false,
-      type: "GET",
-      dataType: "json",
-      success:function(result) {
-          var locations = result.response.groups[0].items;
-          for (var i = 0; i < locations.length; i++) {
-              setMarker(locations[i]);
-          }
-      }
-   });
-  function setMarker(data)
-  {
-    var infowindow = new google.maps.InfoWindow({
-      content: data.venue.name
-    });
-    var myLatLng = {lat: data.venue.location.labeledLatLngs[0].lat, lng: data.venue.location.labeledLatLngs[0].lng};
-    var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-    });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
-  }
+  var myLatLng = {lat: data.venue.location.labeledLatLngs[0].lat, lng: data.venue.location.labeledLatLngs[0].lng};
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+};
+
+var viewModel = function(){
+  var self = this;
+  self.locations = ko.observableArray([
+      {name: "Cleveland", latitude:41.48 , longitude:-81.67},
+      {name: "Chicago", latitude: 41.88, longitude: -87.63}
+  ]);
 }
+
+ko.applyBindings(viewModel);
