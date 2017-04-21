22ed859c417a28e0e2f440bd02d169caf5eaaf63
Jeffery Graham <jefferygraham992@gmail.com>
2017-04-21 13:49:45 -0400

    feat: Add map styling

--- js/app.js
+++ js/app.js
@@ -7,13 +7,116 @@ var url = "https://api.foursquare.com/v2/venues/explore?client_id=JG3FXNYMAHZG1O
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: washingtonDC,
-    zoom: 13
+    zoom: 13,
+    styles: [
+        {
+            "featureType": "landscape",
+            "stylers": [
+                {
+                    "hue": "#F1FF00"
+                },
+                {
+                    "saturation": -27.4
+                },
+                {
+                    "lightness": 9.4
+                },
+                {
+                    "gamma": 1
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "stylers": [
+                {
+                    "hue": "#0099FF"
+                },
+                {
+                    "saturation": -20
+                },
+                {
+                    "lightness": 36.4
+                },
+                {
+                    "gamma": 1
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "stylers": [
+                {
+                    "hue": "#00FF4F"
+                },
+                {
+                    "saturation": 0
+                },
+                {
+                    "lightness": 0
+                },
+                {
+                    "gamma": 1
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "stylers": [
+                {
+                    "hue": "#FFB300"
+                },
+                {
+                    "saturation": -38
+                },
+                {
+                    "lightness": 11.2
+                },
+                {
+                    "gamma": 1
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "stylers": [
+                {
+                    "hue": "#00B6FF"
+                },
+                {
+                    "saturation": 4.2
+                },
+                {
+                    "lightness": -63.4
+                },
+                {
+                    "gamma": 1
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "stylers": [
+                {
+                    "hue": "#9FFF00"
+                },
+                {
+                    "saturation": 0
+                },
+                {
+                    "lightness": 0
+                },
+                {
+                    "gamma": 1
+                }
+            ]
+        }
+    ]
   });
 };
 
 var viewModel = function(){
   var self = this;
-
   //Get array of places from FourSquare API to build list view
   self.locations = ko.observableArray([]);
   $.getJSON(url, function(data) {
@@ -55,7 +158,8 @@ var viewModel = function(){
     markerItem = markers[index];
     if (markerItem.getAnimation() !== null) {
       markerItem.setAnimation(null);
-    } else {
+    }
+    else {
       markerItem.setAnimation(google.maps.Animation.BOUNCE);
     }
   }
