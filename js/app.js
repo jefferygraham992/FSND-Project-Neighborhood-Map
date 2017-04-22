206732c704ec6eb4265fa6d2e7bbad6d6fe2f808
Jeffery Graham <jefferygraham992@gmail.com>
2017-04-21 22:07:44 -0400

    refactor: Make function to build list a viewModel f(x)

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 var map, marker;
 var markers = [];
 var search = "parks";
+var url = "https://api.foursquare.com/v2/venues/explore?client_id=JG3FXNYMAHZG1OVUMBZACXPP3CBVLNT2X1O0BXKGOZKRO4SA%20&client_secret=XI2JWF5HUU2CUOLITHDB2NUZ3EZXEIYML5PVCOG12IZIWNU5%20&v=20130815%20&ll=38.904174,-77.017021&query=" + search +"&radius=10000&limit=10";
 $("#h3-search").text(search[0].toUpperCase() + search.slice(1));
 $("#search").attr("placeholder", (search[0].toUpperCase() + search.slice(1)));
 var washingtonDC = {lat: 38.904174, lng: -77.017021};
-var url = "https://api.foursquare.com/v2/venues/explore?client_id=JG3FXNYMAHZG1OVUMBZACXPP3CBVLNT2X1O0BXKGOZKRO4SA%20&client_secret=XI2JWF5HUU2CUOLITHDB2NUZ3EZXEIYML5PVCOG12IZIWNU5%20&v=20130815%20&ll=38.904174,-77.017021&query=" + search +"&radius=10000&limit=10";
+
+var initialLocations = $.getJSON(url, function(data) {
+  var locations = data.response.groups[0].items;
+  buildList(locations);
+});
+
 
 //Initialize map
 function initMap() {
@@ -120,32 +126,13 @@ function initMap() {
 
 var viewModel = function(){
   var self = this;
-  //Get array of places from FourSquare API to build list view
-  self.locations = ko.observableArray([]);
-  $.getJSON(url, function(data) {
-    var locationsList = data.response.groups[0].items;
-    self.locations(locationsList);
-    //Place markers on map using the array of places retrieved from FourSquare
-    //API
-    for (var i = 0; i < locationsList.length; i++) {
-      // Get the position & name of location from the location array.
-      var position = {lat: locationsList[i].venue.location.lat,
-                      lng: locationsList[i].venue.location.lng};
-      var title = locationsList[i].venue.name;
-      // Create a marker per location, and put into markers array.
-      var marker = new google.maps.Marker({
-        map: map,
-        position: position,
-        title: title,
-        animation: google.maps.Animation.DROP,
-        id: i
-      });
-      //Add listener to add bounce to selected marker
-      marker.addListener('click', toggleBounce);
-      // Push the marker to our array of markers.
-      markers.push(marker);
-    }
-  });
+  //Initialize ko.observableArray for locations
+  this.locationList = ko.observableArray([]);
+
+  //Add locations to ko.observable
+  this.buildList = function(locationArray) {
+    self.locationList(locationArray);
+  };
 
   //Function for toggling markers between bounce/non-bounce state when marker
   //is clicked
@@ -168,6 +155,11 @@ var viewModel = function(){
       markerItem.setAnimation(google.maps.Animation.BOUNCE);
     }
   }
+
+  //Filter list viewModel
+  self.filter = ko.computed(function() {
+
+  });
 }
 
 ko.applyBindings(viewModel);
