4231c5be5372bc2ca73322378b42a9bebad687e8
Jeffery Graham <jefferygraham992@gmail.com>
2017-04-21 23:28:06 -0400

    refactor: Make marker place a viewModel f(x)

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
 var map, marker;
 var markers = [];
 var search = "parks";
+var washingtonDC = {lat: 38.904174, lng: -77.017021};
 var url = "https://api.foursquare.com/v2/venues/explore?client_id=JG3FXNYMAHZG1OVUMBZACXPP3CBVLNT2X1O0BXKGOZKRO4SA%20&client_secret=XI2JWF5HUU2CUOLITHDB2NUZ3EZXEIYML5PVCOG12IZIWNU5%20&v=20130815%20&ll=38.904174,-77.017021&query=" + search +"&radius=10000&limit=10";
 $("#h3-search").text(search[0].toUpperCase() + search.slice(1));
 $("#search").attr("placeholder", (search[0].toUpperCase() + search.slice(1)));
-var washingtonDC = {lat: 38.904174, lng: -77.017021};
 
-var initialLocations = $.getJSON(url, function(data) {
+//AJAX request to build list & place markers
+$.getJSON(url, function(data) {
   var locations = data.response.groups[0].items;
   buildList(locations);
+  placeMarkers();
 });
 
 
 //Initialize map
-function initMap() {
+var initMap = function() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: washingtonDC,
     zoom: 13,
@@ -134,6 +136,24 @@ var viewModel = function(){
     self.locationList(locationArray);
   };
 
+  //Add markers function
+  this.placeMarkers = function() {
+    self.locationList().forEach(function(location, i) {
+      var position = {lat: location.venue.location.lat,
+                      lng: location.venue.location.lng};
+      var title = location.venue.name;
+      marker = new google.maps.Marker({
+        map: map,
+        title: title,
+        position: position,
+        animation: google.maps.Animation.DROP,
+        id: i
+      });
+      marker.addListener('click', toggleBounce);
+      markers.push(marker);
+    });
+  };
+
   //Function for toggling markers between bounce/non-bounce state when marker
   //is clicked
   self.toggleBounce = function (marker) {
@@ -155,11 +175,6 @@ var viewModel = function(){
       markerItem.setAnimation(google.maps.Animation.BOUNCE);
     }
   }
-
-  //Filter list viewModel
-  self.filter = ko.computed(function() {
-
-  });
 }
 
 ko.applyBindings(viewModel);
