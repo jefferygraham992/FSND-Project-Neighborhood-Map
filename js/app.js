fd8fd5fbe80141b8cbcbadf835d42651d3ea70fe
Jeffery Graham <jefferygraham992@gmail.com>
2017-04-29 11:09:40 -0400

    refactor: Refactor slide menu to slide list on/off page w/o moving page

--- js/app.js
+++ js/app.js
@@ -6,11 +6,47 @@ var url = "https://api.foursquare.com/v2/venues/explore?client_id=JG3FXNYMAHZG1O
 $("#h3-search").text(search[0].toUpperCase() + search.slice(1));
 $("#search").attr("placeholder", (search[0].toUpperCase() + search.slice(1)));
 
+function ListControl(controlDiv, map) {
+
+  // Set CSS for the control border.
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = '#fff';
+  controlUI.style.border = '2px solid #fff';
+  controlUI.style.borderRadius = '3px';
+  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.marginBottom = '22px';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'Click to recenter the map';
+  controlDiv.appendChild(controlUI);
+
+  // Set CSS for the control interior.
+  var controlText = document.createElement('div');
+  controlText.style.color = 'rgb(25,25,25)';
+  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlText.style.fontSize = '16px';
+  controlText.style.lineHeight = '38px';
+  controlText.style.paddingLeft = '5px';
+  controlText.style.paddingRight = '5px';
+  controlText.innerHTML = 'Open Locations';
+  controlUI.appendChild(controlText);
+
+  // Setup the click event listeners: simply set the map to Chicago.
+  controlUI.addEventListener('click', function() {
+    openNav();
+  });
+};
+
 //Initialize map
 var initMap = function() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: washingtonDC,
     zoom: 13,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+              style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+              position: google.maps.ControlPosition.TOP_RIGHT
+          },
     styles: [
         {
             "featureType": "landscape",
@@ -116,6 +152,13 @@ var initMap = function() {
         }
     ]
   });
+  // Create the DIV to hold the control and call the CenterControl()
+  // constructor passing in this DIV.
+  var listControlDiv = document.createElement('div');
+  var listControl = new ListControl(listControlDiv, map);
+
+  listControlDiv.index = 1;
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(listControlDiv);
 };
 
 var viewModel = function(){
